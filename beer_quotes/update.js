a012bc6e2a95acd70c110e057799600329373fa5
Duong Tran <duong.tran@deepend.melbourne>
2017-08-31 15:07:58 +1000

    Serverless beers

---
+++ beer_quotes/update.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.update = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  // validation
+  if (typeof data.text !== 'string' || typeof data.checked !== 'boolean') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t update the quote item.'));
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+    ExpressionAttributeNames: {
+      '#quote': 'quote',
+    },
+    ExpressionAttributeValues: {
+      ':quote': data.quote,
+      ':updatedAt': timestamp,
+    },
+    UpdateExpression: 'SET #quote = :quote, updatedAt = :updatedAt',
+    ReturnValues: 'ALL_NEW',
+  };
+
+  // update the todo in the database
+  dynamoDb.update(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t update the todo item.'));
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Attributes),
+    };
+    callback(null, response);
+  });
+};
